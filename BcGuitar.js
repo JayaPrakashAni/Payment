e4fac6edf1b4f7ad6d752a6ab2c16c24794626d1
JayaPrakashAni <146164179+JayaPrakashAni@users.noreply.github.com>
2024-01-27 21:12:08 +0530

    payment app >> guitar

---
+++ BcGuitar.js
@@ -0,0 +1,85 @@
+import React, { useEffect, useState } from "react";
+import { View, Text, ImageBackground, ActivityIndicator, ScrollView, StyleSheet, Image } from "react-native";
+
+const BCGuitar = ({ }) => {
+  const [dataArray, setDataArray] = useState([]);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const imagebackground = require('../Images/background/blackbg.jpeg');
+
+
+  const getUserData = async () => {
+    try {
+      const response = await fetch("https://my.bmusician.com/app/getcourses/1");
+      const myData = await response.json();
+      if (myData && myData.CourseList) {
+        setDataArray([myData.CourseList]); // Assuming coursedetails is an object, not an array
+      }
+      setIsLoaded(true);
+      console.log('Resultuuu>>>>', myData);
+      // console.log('value>>>', myData?.CourseList[0].CourseName);
+      //console.log('value>>>', myData?.CourseList[0].ID);
+      // console.log('value>>>', myData?.CourseList[0].CourseDescription);
+
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getUserData();
+
+  }, []);
+
+  if (!isLoaded) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <ActivityIndicator size={'large'} />
+      </View>
+    );
+  }
+
+  return (
+    <ImageBackground source={imagebackground} style={{ width: '100%', height: '100%' }}>
+      <ScrollView>
+        <View>
+          
+          <View style={{ flex: 1, }}>
+            {
+              dataArray.map((item) => (
+                <View>
+                  <Text style={styles.Item} key={item.ID}>
+                    {item[1].CourseName}
+                  </Text>
+
+             
+          
+                  <Text style={{ fontSize: 25, marginLeft: 20, color: 'white', marginTop: 30, }}>Course Description</Text>
+                  <Text style={{ fontSize: 25, marginLeft: 20, color: 'white', marginTop: 30 }} key={item.id}>
+                    {item[1].CourseDescription}
+                  </Text>
+
+                </View>
+              ))
+            }
+          </View>
+        </View>
+      </ScrollView>
+    </ImageBackground>
+  );
+};
+
+
+const styles = StyleSheet.create({
+  Item: {
+    fontSize: 25,
+    margin: 5,
+    color: 'red',
+    padding: 5,
+    textAlign: 'center',
+
+  }
+});
+
+
+
+export default BCGuitar;
